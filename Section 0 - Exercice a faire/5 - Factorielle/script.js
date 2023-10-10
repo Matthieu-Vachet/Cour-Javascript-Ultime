500e6676feab68e3498e79e3ebb319d9c6ff968f
Matthieu-Vachet <79032023+Matthieu-Vachet@users.noreply.github.com>
2023-10-11 01:44:38 +0200

    Update script.js

--- Section 0 - Exercice a faire/5 - Factorielle/script.js
+++ Section 0 - Exercice a faire/5 - Factorielle/script.js
@@ -1,1 +1,19 @@
 // Objectif : Créez une variable factorielle. Ensuite, créez une fonction calculerFactorielle qui prend un nombre en entrée et renvoie sa factorielle.
+
+// Exemple :
+// 5! = 5 x 4 x 3 x 2 x 1 = 120
+
+let factoriel = 1;
+let nombre = 5;
+
+function calculerLeFactoriel(nombre) {
+  for (let i = 1; i <= nombre; i++) {
+    factoriel *= i;
+		console.log(factoriel);
+  }
+  return factoriel;
+}
+
+let result = calculerLeFactoriel(nombre);
+
+console.log("La factoriel de " + nombre + " est : " + result);
