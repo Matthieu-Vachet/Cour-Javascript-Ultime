bf0e9707b1865b9f17dba8eac36914ca9939abb2
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2023-12-21 17:06:35 +0100

    Add DOM manipulation code and stylesheets

---
+++ Section 13 - Communiquons avec un site internet/1 - Accéder au élément du DOM/script.js
@@ -0,0 +1,19 @@
+// Récupérer une balise du DOM
+let header = document.getElementsByTagName("header")
+
+// Récupérer un Id du DOM
+let logo = document.getElementById("logo")
+
+// Récupérer une classe du DOM
+let container = document.getElementsByClassName("container")
+
+// Récupérer le premier sélecteur h1 du DOM
+let h1 = document.querySelector("h1") 
+
+// Récupérer tous les sélecteurs p du DOM
+let p = document.querySelectorAll("p")
+
+console.log(header)
+console.log(logo)
+console.log(container)
+console.log(h1)
---
+++ Section 13 - Communiquons avec un site internet/2 - Modifier des éléments du DOM (textContent, innerHTML)/script.js
@@ -0,0 +1,8 @@
+let h1 = document.querySelector('h1');
+h1.textContent = "Hello World";
+h1.innerHTML = "<div style='color: red'>Hello World</div>";
+
+console.log(h1);
+
+// Toute les propriétés et méthodes du DOM sont disponible sur le site de Mozilla
+// https://developer.mozilla.org/fr/docs/Web/API/Element
